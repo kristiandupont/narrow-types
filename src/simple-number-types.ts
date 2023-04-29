efbf3707e4bea78be042cf13cf89b0502284ff3e
Kristian Dupont <kristian@kristiandupont.com>
2023-04-29 09:05:49 +0000

    Add various precision ints

--- src/simple-number-types.ts
+++ src/simple-number-types.ts
@@ -5,6 +5,26 @@ import Branded from './Branded';
 export type Integer = Branded<number, 'Integer'>;
 export const integer: z.Schema<Integer> = z.number().int() as any;
 
+export type Int2 = Branded<number, 'Int2'>;
+export const int2: z.Schema<Int2> = z
+  .number()
+  .int()
+  .min(-32_768)
+  .max(32_767) as any;
+
+export type Int4 = Branded<number, 'Int4'>;
+export const int4: z.Schema<Int4> = z
+  .number()
+  .int()
+  .min(-2_147_483_648)
+  .max(2_147_483_647) as any;
+
+export type Int8 = Branded<bigint, 'Int8'>;
+export const int8: z.Schema<Int8> = z
+  .bigint()
+  .min(-9_223_372_036_854_775_808n)
+  .max(9_223_372_036_854_775_807n) as any;
+
 export type PositiveNumber = Branded<number, 'PositiveNumber'>;
 export const positiveNumber: z.Schema<PositiveNumber> = z
   .number()
