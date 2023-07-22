0dcd370a641e0151d6f80272b73e55f2df000512
Kristian Dupont <kristian@kristiandupont.com>
2023-07-22 08:00:38 +0000

    Use Zod-native branding

--- src/Branded.ts
+++ src/Branded.ts
@@ -1,6 +1,5 @@
-class Tagged<Tag> {
-  private __tag!: Tag;
-}
-type Branded<T, Tag> = T & Tagged<Tag>;
+import { BRAND } from 'zod';
+
+type Branded<T, Tag extends string | number | symbol> = T & BRAND<Tag>;
 
 export default Branded;
