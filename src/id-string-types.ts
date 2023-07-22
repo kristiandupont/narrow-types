50ce81ee3d8a5fcaf4a43c0dbe11e021731165ef
Kristian Dupont <kristian@kristiandupont.com>
2023-07-22 07:32:52 +0000

    Whoops!

--- src/id-string-types.ts
+++ src/id-string-types.ts
@@ -2,14 +2,14 @@ import { z } from 'zod';
 
 import Branded from './Branded';
 
-export type UUIDString = Branded<number, 'UUIDString'>;
+export type UUIDString = Branded<string, 'UUIDString'>;
 export const uuidString: z.Schema<UUIDString> = z.string().uuid() as any;
 
-export type CUIDString = Branded<number, 'CUIDString'>;
+export type CUIDString = Branded<string, 'CUIDString'>;
 export const cuidString: z.Schema<CUIDString> = z.string().cuid() as any;
 
-export type CUID2String = Branded<number, 'CUID2String'>;
+export type CUID2String = Branded<string, 'CUID2String'>;
 export const cuid2String: z.Schema<CUIDString> = z.string().cuid2() as any;
 
-export type ULIDString = Branded<number, 'ULIDString'>;
+export type ULIDString = Branded<string, 'ULIDString'>;
 export const ulidString: z.Schema<ULIDString> = z.string().ulid() as any;
--- src/simple-string-types.ts
+++ src/simple-string-types.ts
@@ -2,15 +2,15 @@ import { z } from 'zod';
 
 import Branded from './Branded';
 
-export type EmojiString = Branded<number, 'EmojiString'>;
+export type EmojiString = Branded<string, 'EmojiString'>;
 export const emojiString: z.Schema<EmojiString> = z.string().emoji() as any;
 
-export type DateTimeString = Branded<number, 'DateTimeString'>;
+export type DateTimeString = Branded<string, 'DateTimeString'>;
 export const dateTimeString: z.Schema<DateTimeString> = z
   .string()
   .datetime() as any;
 
-export type JSONString = Branded<number, 'JsonString'>;
+export type JSONString = Branded<string, 'JsonString'>;
 export const jsonString: z.Schema<JSONString> = z.string().refine((value) => {
   try {
     JSON.parse(value);
